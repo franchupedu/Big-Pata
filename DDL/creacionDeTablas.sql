22320548fafb008d85aaa15d834136e1e2541668
Hernan <hernan_rodriguez_2013@hotmail.com>
2020-05-23 19:20:24 -0300

    cambio toda la creacion de tablas

--- DDL/creacionDeTablas.sql
+++ DDL/creacionDeTablas.sql
@@ -8,8 +8,12 @@ if object_id('SELECT_QUANTUM_LIBRARY.Nota_De_Venta') is not null
 	DROP TABLE SELECT_QUANTUM_LIBRARY.Nota_De_Venta;
 if object_id('SELECT_QUANTUM_LIBRARY.Compra') is not null
 	DROP TABLE SELECT_QUANTUM_LIBRARY.Compra;
+if object_id('SELECT_QUANTUM_LIBRARY.Empresa') is not null
+	DROP TABLE SELECT_QUANTUM_LIBRARY.Empresa;
 if object_id('SELECT_QUANTUM_LIBRARY.Ruta_Aerea') is not null
 	DROP TABLE SELECT_QUANTUM_LIBRARY.Ruta_Aerea;
+if object_id('SELECT_QUANTUM_LIBRARY.Ciudad') is not null
+	DROP TABLE SELECT_QUANTUM_LIBRARY.Ciudad;
 if object_id('SELECT_QUANTUM_LIBRARY.Vuelo') is not null
 	DROP TABLE SELECT_QUANTUM_LIBRARY.Vuelo;
 if object_id('SELECT_QUANTUM_LIBRARY.Habitacion') is not null
@@ -50,7 +54,7 @@ PRIMARY KEY(codigo)
 
 
 CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Avion](
-[id_avion] [nvarchar] (255),
+[id_avion] [nvarchar] (255) NOT NULL,
 [modelo] [nvarchar] (255) NOT NULL,
 PRIMARY KEY(id_avion)
 );
@@ -59,7 +63,9 @@ CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Butaca](
 [id_butaca] [int] IDENTITY(1,1),
 [tipo_de_butaca] [int] NOT NULL,
 [numero] [int] NOT NULL,
+[id_avion] [nvarchar] (255) NOT NULL
 FOREIGN KEY (tipo_de_butaca) REFERENCES [SELECT_QUANTUM_LIBRARY].[Tipo_Butaca](codigo),
+FOREIGN KEY (id_avion) REFERENCES [SELECT_QUANTUM_LIBRARY].[Avion](id_avion),
 PRIMARY KEY(id_butaca)
 );
 
@@ -92,13 +98,13 @@ PRIMARY KEY(id_hotel)
 
 CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Habitacion](
 [id_habitacion] [int] IDENTITY(1,1),
+[id_hotel] [int] not NULL,
 [piso] [int] NOT NULL,
 [numero][int] NOT NULL,
 [tipo] [int] NOT NULL, 
 [frente] [nvarchar] (255) NOT NULL, 
-[costo_compra] [int] NOT NULL,
-[precio_venta] [int] NOT NULL
-FOREIGN KEY(tipo) REFERENCES [SELECT_QUANTUM_LIBRARY].[Tipo_Habitacion](codigo) 
+FOREIGN KEY(id_hotel) REFERENCES [SELECT_QUANTUM_LIBRARY].[Hotel](id_hotel),
+FOREIGN KEY(tipo) REFERENCES [SELECT_QUANTUM_LIBRARY].[Tipo_Habitacion](codigo),
 PRIMARY KEY(id_habitacion)
 );
 
@@ -111,62 +117,81 @@ FOREIGN KEY(id_avion) REFERENCES [SELECT_QUANTUM_LIBRARY].[Avion],
 PRIMARY KEY (codigo_vuelo)
 );
 
+CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Ciudad](
+[id_ciudad] [int] IDENTITY(1,1),
+[nombre] [nvarchar] (255) NOT NULL,
+PRIMARY KEY(id_ciudad)
+);
+
 CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Ruta_Aerea](
 [id_ruta_aerea] [int] IDENTITY(1,1),
 [codigo_ruta_aerea] [int],
-[ciudad_origen] [nvarchar] (255) NOT NULL,
-[ciudad_destino] [nvarchar] (255) NOT NULL,
-[id_vuelo] [int],
+[ciudad_origen] [int] NOT NULL,
+[ciudad_destino] [int] NOT NULL,
+[id_vuelo] [int] NOT NULL,
 FOREIGN KEY (id_vuelo) REFERENCES [SELECT_QUANTUM_LIBRARY].[Vuelo](codigo_vuelo),
+FOREIGN KEY (ciudad_origen) REFERENCES [SELECT_QUANTUM_LIBRARY].[Ciudad](id_ciudad),
+FOREIGN KEY (ciudad_destino) REFERENCES [SELECT_QUANTUM_LIBRARY].[Ciudad](id_ciudad),
 PRIMARY KEY(id_ruta_aerea)
 );
 
-CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Compra](
-[numero] [int] IDENTITY(1,1),
-[fecha] [date] NOT NULL,
-[empresa] [nvarchar] (255),
-[costo_total] [int] NOT NULL,
-PRIMARY KEY (numero)
+CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Empresa](
+[id_empresa] [int] IDENTITY(1,1),
+[nombre] [nvarchar] (255) NOT NULL,
+PRIMARY KEY(id_empresa)
 );
+
+CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Compra](
+[id_compra][int]IDENTITY(1,1),
+[fecha] [date] NOT NULL,
+[id_empresa][int] NOT NULL,
+[costo_total][int] NOT NULL,
+FOREIGN KEY (id_empresa) REFERENCES[SELECT_QUANTUM_LIBRARY].[Empresa](id_empresa),
+PRIMARY KEY (id_compra)
+)
+
+
 
 CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Nota_De_Venta](
 [id_nota_de_venta][int]IDENTITY(1,1),
-[id_cliente][int] NOT NULL,
+[id_cliente] [int] NOT NULL,
 [id_sucursal] [int] NOT NULL,
 [fecha_de_salida] [date] NOT NULL,
-[id_butaca] [int] NOT NULL,
 [precio_total] [int] NOT NULL,
 FOREIGN KEY (id_cliente) REFERENCES[SELECT_QUANTUM_LIBRARY].[Cliente](DNI),
 FOREIGN KEY (id_sucursal) REFERENCES[SELECT_QUANTUM_LIBRARY].[Sucursal](id_sucursal),
 PRIMARY KEY (id_nota_de_venta)
-);
+)
 
 CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Pasaje](
 [codigo_pasaje] [int] IDENTITY(1,1),
 [codigo_vuelo] [int] NOT NULL,
-[precio_venta] [int] NOT NULL,
-[costo_compra] [int] NOT NULL,
 [fecha_compra] [date] NOT NULL,
 [id_butaca] [int] NOT NULL,
 [id_compra] [int] NOT NULL,
+[costo_compra] [int] NOT NULL,
+[id_nota_de_venta] [int] NOT NULL,
+[precio_venta] [int] NOT NULL,
 FOREIGN KEY(codigo_vuelo) REFERENCES [SELECT_QUANTUM_LIBRARY].[Vuelo](codigo_vuelo),
-FOREIGN KEY(costo_compra) REFERENCES [SELECT_QUANTUM_LIBRARY].[Compra](numero),
 FOREIGN KEY(id_butaca) REFERENCES [SELECT_QUANTUM_LIBRARY].[Butaca](id_butaca),
-FOREIGN KEY(id_compra) REFERENCES [SELECT_QUANTUM_LIBRARY].[Nota_De_Venta](id_nota_de_venta),
+FOREIGN KEY(id_compra) REFERENCES [SELECT_QUANTUM_LIBRARY].[Compra](id_compra),
+FOREIGN KEY(id_nota_de_venta) REFERENCES [SELECT_QUANTUM_LIBRARY].[Nota_De_Venta](id_nota_de_venta),
 PRIMARY KEY(codigo_pasaje)
 );
 
 
 CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Estadia](
-[codigo] [int] IDENTITY(1,1),
+[id_estadia] [int] IDENTITY(1,1),
+[codigo] [int] NOT NULL,
 [fecha_inicio] [date] NOT NULL,
 [cantidad_noches] [int] NOT NULL,
+[id_habitacion] [int] NOT NULL,
 [id_compra] [int] NOT NULL,
-[id_hotel] [int] NOT NULL,
 [precio_final] [int] NOT NULL,
-FOREIGN KEY(id_compra) REFERENCES [SELECT_QUANTUM_LIBRARY].[Compra](numero),
-FOREIGN KEY(id_hotel) REFERENCES [SELECT_QUANTUM_LIBRARY].[Hotel](id_hotel),
-PRIMARY KEY (codigo) 
+[costo_compra_total] [int] NOT NULL,
+FOREIGN KEY(id_habitacion) REFERENCES [SELECT_QUANTUM_LIBRARY].[Habitacion](id_habitacion),
+FOREIGN KEY(id_compra) REFERENCES [SELECT_QUANTUM_LIBRARY].[Compra](id_compra),
+PRIMARY KEY (id_estadia) 
 );
 
 
@@ -174,10 +199,10 @@ CREATE TABLE [SELECT_QUANTUM_LIBRARY].[Dia_Reservado](
 [codigo] [int] IDENTITY(1,1),
 [id_estadia] [int] NOT NULL,
 [fecha] [date] NOT NULL,
-[id_habitacion] [int] NOT NULL,
+[precio_venta_dia] [int] NOT NULL,
+[costo_compra_dia] [int] NOT NULL,
 [id_nota_de_venta] [int],
 FOREIGN KEY(id_nota_de_venta) REFERENCES[SELECT_QUANTUM_LIBRARY].[Nota_De_Venta](id_nota_de_venta),
-FOREIGN KEY(id_estadia) REFERENCES [SELECT_QUANTUM_LIBRARY].[Estadia](codigo),
-FOREIGN KEY(id_habitacion) REFERENCES [SELECT_QUANTUM_LIBRARY].[Habitacion](id_habitacion),
+FOREIGN KEY(id_estadia) REFERENCES [SELECT_QUANTUM_LIBRARY].[Estadia](id_estadia),
 PRIMARY KEY(codigo)
 );
