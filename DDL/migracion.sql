a6f41cad9a3d1a5aa4e7d9f8e436663680a82ce8
Leonel <leomorsella@est.frba.utn.edu.ar>
2020-05-24 14:34:52 -0300

    se agregan algunas tablas a la migracion

--- DDL/migracion.sql
+++ DDL/migracion.sql
@@ -1,5 +1,17 @@
 use GD1C2020
 
+/*Estan faltando las siguientes tablas (por favor actualizar a medida que se avanza
+Ciudad
+Pasaje
+Compra
+Estadia
+Dia Reservado
+Nota de Venta
+Dia Reservado
+*/
+
+/*para probar*/
+SELECT * FROM gd_esquema.Maestra
 select count(*)
 from gd_esquema.Maestra where HABITACION_PISO is not null
 group by HOTEL_CALLE, HOTEL_NRO_CALLE, HABITACION_PISO, HABITACION_COSTO, HABITACION_FRENTE, HABITACION_NUMERO, 
@@ -8,16 +20,29 @@ HABITACION_PRECIO, TIPO_HABITACION_CODIGO, TIPO_HABITACION_DESC
 select AVION_IDENTIFICADOR, AVION_MODELO from gd_esquema.Maestra
 
 
+
+/*inicio de la migracion*/
+
+
 /* Tipo de Habitacion*/
 insert into [SELECT_QUANTUM_LIBRARY].[Tipo_Habitacion] select 
 														distinct(TIPO_HABITACION_CODIGO),
 														TIPO_HABITACION_DESC 
 														FROM gd_esquema.Maestra 
 														WHERE TIPO_HABITACION_CODIGO is not null 
-
+/*Hotel*/
+insert into [SELECT_QUANTUM_LIBRARY].[Hotel] SELECT 
+													HOTEL_CALLE,
+													HOTEL_NRO_CALLE,
+													HOTEL_CANTIDAD_ESTRELLAS
+													FROM gd_esquema.Maestra
+													WHERE HOTEL_CALLE IS NOT NULL
+													
+													
 
 /*Habitacion*/	/*XXX: puse distinct aca*/
 insert into [SELECT_QUANTUM_LIBRARY].[Habitacion] SELECT 
+														distinct(SELECT id_hotel FROM [SELECT_QUANTUM_LIBRARY].[Hotel]),
 														HABITACION_PISO,
 														HABITACION_NUMERO,
 														(select codigo from[SELECT_QUANTUM_LIBRARY].[Tipo_Habitacion] 
@@ -39,6 +64,7 @@ insert into [SELECT_QUANTUM_LIBRARY].[Tipo_Butaca] select
 													distinct(BUTACA_TIPO) 
 													FROM gd_esquema.Maestra
 													WHERE BUTACA_TIPO is not null
+
 
 /*Avion*/
 insert into [SELECT_QUANTUM_LIBRARY].[Avion] SELECT 
@@ -84,7 +110,7 @@ insert into [SELECT_QUANTUM_LIBRARY].[Butaca] select
 													FROM gd_esquema.Maestra
 													WHERE BUTACA_NUMERO is not null
 													Order by 1, 2
-
+/*Sucursal*/
 INSERT INTO [SELECT_QUANTUM_LIBRARY].[Sucursal]	select 
 													DISTINCT SUCURSAL_MAIL,
 													SUCURSAL_DIR,
@@ -93,15 +119,17 @@ INSERT INTO [SELECT_QUANTUM_LIBRARY].[Sucursal]	select
 													WHERE SUCURSAL_MAIL is not null							
 
 
-/*Pasaje*/ 
-/*Compra*/
+/*Cliente*/
+INSERT INTO [SELECT_QUANTUM_LIBRARY].[Cliente] SELECT
+													distinct CLIENTE_DNI,
+													CLIENTE_NOMBRE,
+													CLIENTE_APELLIDO,
+													CLIENTE_MAIL,
+													CLIENTE_TELEFONO,
+													CLIENTE_FECHA_NAC
+													FROM gd_esquema.Maestra
+													WHERE CLIENTE_DNI IS NOT NULL
 
-/*Estadia*/
 
-/*Dia Reservado*/
-
-															
-
-
-
+														
 
